abe9a6fd2bfb6005d6cae5efb9319ffee0b667b1
yuunik <chorria.zhou@gmail.com>
2025-01-01 22:58:16 +0800

    feat: 美食制作流程-制作流程渲染完成。

--- lib/ui/pages/detail/detail_content.dart
+++ lib/ui/pages/detail/detail_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_court/core/model/meal_model.dart';
 import 'package:food_court/core/extension/int_fit.dart';
+import 'package:food_court/ui/shared/size_fit.dart';
 
 class MealDetailContent extends StatelessWidget {
   final MealModel _mealInfo;
@@ -19,12 +20,13 @@ class MealDetailContent extends StatelessWidget {
           buildMenuMaterial(),
           buildItemTitle(context, "制作流程"),
           // 菜单制作步骤
-          buildMenuStep()
+          buildMenuStep(context)
         ],
       ),
     );
   }
 
+  // 菜单图片
   Widget buildMenuImage() {
     return Container(
       child: Image.network(
@@ -33,44 +35,53 @@ class MealDetailContent extends StatelessWidget {
     );
   }
 
+  // 菜单材料说明
   Widget buildMenuMaterial() {
-    return Padding(
-      padding: EdgeInsets.all(10.px),
-      child: Container(
-        padding: EdgeInsets.all(8.px),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.grey,
-            width: 2.px,
-          ),
-          borderRadius: BorderRadius.circular(10.px)
-        ),
-        child: ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: _mealInfo.ingredients.length,
-          itemBuilder: (context, index) => Card(
+    return buildListViewBox(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: _mealInfo.ingredients.length,
+        itemBuilder: (context, index) => Card(
             color: Colors.amberAccent,
             child: Padding(
               padding: EdgeInsets.all(10.px),
               child: Text(
                 _mealInfo.ingredients[index],
                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.w400
+                    fontWeight: FontWeight.w400
                 ),
               ),
             )
-          ),
         ),
       ),
     );
   }
 
-  Widget buildMenuStep() {
-    return Text("制作流程");
+  // 菜单制作步骤
+  Widget buildMenuStep(BuildContext context) {
+    return buildListViewBox(
+      child: ListView.separated(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: _mealInfo.steps.length,
+        itemBuilder: (context, index) => ListTile(
+          leading: CircleAvatar(
+            child: Text(
+                "# $index"
+            ),
+          ),
+          title: Text(
+              _mealInfo.steps[index]
+          ),
+        ),
+        separatorBuilder: (context, index) => const Divider(),
+      ),
+    );
   }
 
+  // 标题
   Widget buildItemTitle(BuildContext context, String title) {
     return Padding(
       padding: EdgeInsets.symmetric(
@@ -84,4 +95,23 @@ class MealDetailContent extends StatelessWidget {
       ),
     );
   }
+
+  // 构建列表视图盒子
+  Widget buildListViewBox({required Widget child}) => Padding(
+    padding: EdgeInsets.all(10.px),
+    child: Container(
+      padding: EdgeInsets.all(
+        8.px
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.px),
+        border: Border.all(
+          color: Colors.grey,
+          width: 2.px
+        )
+      ),
+      child: child,
+    ),
+  );
 }
